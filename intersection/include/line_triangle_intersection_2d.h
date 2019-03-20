0f283486e32d7fa80c8ebc03bfe9b8c7f91605e4
G2X <g2simm@gmail.com>
2019-03-20 21:06:21 +0900

    Rename line to line_segment

--- intersection/include/line_triangle_intersection_2d.h
+++ intersection/include/line_triangle_intersection_2d.h
@@ -17,14 +17,14 @@ bool IsIntersectedLineAndTriangle2D(const std::array<Vec2, 2> &line, const std::
 
 // line := (x1, y1) (x2, y2)
 // triangle := (x1, y1) (x2, y2) (x3, y3)
-bool IsIntersectedLineSegmentAndTriangle2D(const std::array<Vec2, 2> &line, const std::array<Vec2, 3> &triangle)
+bool IsIntersectedLineSegmentAndTriangle2D(const std::array<Vec2, 2> &line_segment, const std::array<Vec2, 3> &triangle)
 {
   std::array<std::array<Vec2, 2>, 3> edges{
       std::array<Vec2, 2>{triangle[0], triangle[1]},
       std::array<Vec2, 2>{triangle[1], triangle[2]},
       std::array<Vec2, 2>{triangle[2], triangle[0]}};
 
-  return IsIntersectedLineSegment2D(line, edges[0]) ||
-         IsIntersectedLineSegment2D(line, edges[1]) ||
-         IsIntersectedLineSegment2D(line, edges[2]);
+  return IsIntersectedLineSegment2D(line_segment, edges[0]) ||
+         IsIntersectedLineSegment2D(line_segment, edges[1]) ||
+         IsIntersectedLineSegment2D(line_segment, edges[2]);
 }
